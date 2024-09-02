43aa4be9e7f0c1a9da1febb2b2008aabbdf5706c
Mackinley <mackinley@punchbuggy.com.au>
2024-09-02 13:02:37 +1000

    init

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <!-- <TheTopNavBar/> -->
+
+  <div class="">
+
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+
+    
+
+  </div>
+
+  <footer class="dark:bg-slate-800 text-center dark:text-white p-8 bg-purple-100 font-bold ">
+      
+    
+      <div>&copy; STOCK OPTIONS PLEASE</div>
+
+  
+    </footer>
+
+</template>
+ 
---
+++ nuxt.config.ts
@@ -0,0 +1,31 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+export default defineNuxtConfig({
+  app: {
+  },
+  vite:{
+    build: {
+      rollupOptions: {
+        external: ["@iconify/vue"]
+      }
+    }
+  },
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt','@nuxtjs/color-mode'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
+  colorMode: {
+    classSuffix: ''
+  }
+
+})
